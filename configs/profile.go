357ff7d3ac42d9779e87e211a8708a22e124ace6
Aung Shan Bo <aungshanbo@workstation.local>
2026-07-19 01:48:47 +0630

    add handling for scanner in profile

--- configs/profile.go
+++ configs/profile.go
@@ -8,31 +8,33 @@ import (
 	"strings"
 )
 
-func Loadconfig() [] string{
-	var profiles [] string
-	homeDir ,err := os.UserHomeDir()
+func Loadconfig() []string {
+	var profiles []string
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println("Error getting home, err")
 	}
 
-	configFile := filepath.Join(homeDir,".aws","config")
+	configFile := filepath.Join(homeDir, ".aws", "config")
 
-	file , err := os.Open(configFile)
+	file, err := os.Open(configFile)
 	if err != nil {
-		fmt.Println("Error getting config file",err)
+		fmt.Println("Error getting config file", err)
 	}
 
 	scanner := bufio.NewScanner(file)
-
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
 
-		if strings.HasPrefix(line, "[") && strings.HasSuffix( line, "]") {
-			line = strings.Trim(line,"[]")
-			line = strings.TrimPrefix(line,"profile ")
-			profiles = append(profiles,line)
+		if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
+			line = strings.Trim(line, "[]")
+			line = strings.TrimPrefix(line, "profile ")
+			profiles = append(profiles, line)
 
 		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading config file", err)
 	}
 	return profiles
 }
